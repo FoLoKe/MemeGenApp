6629fabed6fa926943c8c10eef3c680016ef6830
FoLoKe <FoLoKe@yandex.ru>
2020-02-10 10:18:55 +0300

    aide errors resolve

--- app/src/main/java/com/foloke/memgenactivity/ScrollingActivity.java
+++ app/src/main/java/com/foloke/memgenactivity/ScrollingActivity.java
@@ -4,11 +4,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 
 import com.foloke.memgenactivity.Entities.Greeting;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.util.Log;
 import android.view.View;
@@ -18,51 +14,23 @@ import android.widget.TextView;
 
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+import android.app.*;
 
-public class ScrollingActivity extends AppCompatActivity {
+public class ScrollingActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_scrolling);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                new HttpRequestTask().execute();
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        setContentView(R.layout.main);
+		new HttpRequestTask().execute();
+        
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_scrolling, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
+  
     @Override
     protected void onStart() {
         super.onStart();
+		
     }
 
     private class HttpRequestTask extends AsyncTask<Void, Void, Greeting> {
@@ -72,7 +40,7 @@ public class ScrollingActivity extends AppCompatActivity {
                 final String url = "http://31.42.45.42:10204/greeting?name=SUKA";
                 RestTemplate restTemplate = new RestTemplate();
                 restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
-                Greeting greeting = restTemplate.getForObject(url, Greeting.class);
+                Greeting greeting = (Greeting)restTemplate.getForObject(url, Greeting.class);
                 return greeting;
             } catch (Exception e) {
                 Log.e("MainActivity", e.getMessage(), e);
@@ -83,8 +51,8 @@ public class ScrollingActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(Greeting greeting) {
-            TextView greetingIdText = (TextView) findViewById(R.id.id_value);
-            TextView greetingContentText = (TextView) findViewById(R.id.content_value);
+            TextView greetingIdText = (TextView) findViewById(R.id.one);
+            TextView greetingContentText = (TextView) findViewById(R.id.two);
             if (greeting != null) {
 
                 greetingIdText.setText(greeting.getId());
